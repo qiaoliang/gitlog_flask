39b90cf77c6d32488242f7aab460145e446082d4
qiaoliang <sagittatius.qiao@gmail.com>
2022-10-24 12:13:49 +0800

    add module of CORS for cross-site visiting

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from flask import Flask
+from flask_cors import CORS
 from parser import logParser
+
 class FlaskSiteConfig(object):
     """默认配置"""
     GITLOG_FILE ='./1.txt'
@@ -12,9 +14,17 @@ def create_flask_app(config):
         static_folder="static",         # -> 设定：在 static 目录下存储静态资源
         template_folder="templates")    # -> 设定：在 templates 目录下存储静态资源
 
-    app.config.from_object(config) # -> 设定：从配置类 Config 中读取配置信息    
-    return app
+    app.config.from_object(config) # -> 设定：从配置类 Config 中读取配置信息
+    """
+    CORS(app, origins=None, methods=None, supports_credentials=False)
 
+    origins: 允许来自的来源或原始列表。原点可以是正则表达式，区分大小写的字符串，也可以是星号.
+    允许来源用于非简单请求的方法或方法列表。类似于django中的CORS_ORIGIN_REGEX_WHITELIST；
+    methods: 跨域请求允许的请求方法,相当于django中的CORS_ALLOW_METHODS；
+    supports_credentials: 为True，允许用户进行经过身份验证的请求。允许跨域提交cookie。
+    """
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})  
+    return app
 app = create_flask_app(FlaskSiteConfig)
 
 @app.route('/') 
